8f6538b577adcc36f2aa93a652d0b3f34a485adc
AibaUmamekov <aiba.umambekov@gmail.com>
2021-01-22 09:51:33 +0600

    все сделал по книге

--- educa/urls.py
+++ educa/urls.py
@@ -19,8 +19,10 @@ from django.contrib.auth import views as auth_views
 from courses.views import CourseListView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_swagger.views import get_swagger_view
 
 urlpatterns = [
+
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
@@ -28,6 +30,7 @@ urlpatterns = [
     path('', CourseListView.as_view(), name='course_list'),
     path('students/', include('students.urls')),
     path('api/', include('courses.api.urls', namespace='api')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
